302ddcbfb57a587266fe9636f74b9df7c0ca6354
colton <cgardner268@gmail.com>
2017-11-03 19:54:07 -0700

    Sample Web API wih extra method.

---
+++ WebApiSampleProject/WebApiSampleProject/App_Start/WebApiConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace WebApiSampleProject
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            //All employees or employee by Id.
+            config.Routes.MapHttpRoute(
+                name: "EmployeeDefault",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            //Specific employee by name
+            config.Routes.MapHttpRoute(
+                name: "EmployeeByName",
+                routeTemplate: "api/{controller}/{action}/{name}",
+                defaults: new { name = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ WebApiSampleProject/WebApiSampleProject/Controllers/EmployeeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiSampleProject.Models;
+
+namespace WebApiSampleProject.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        IList<Employee> employees = new List<Employee>()
+        {
+            new Employee()
+                {
+                    EmployeeId = 1, EmployeeName = "Mukesh Kumar", Address = "New Delhi", Department = "IT"
+                },
+                new Employee()
+                {
+                    EmployeeId = 2, EmployeeName = "Banky Chamber", Address = "London", Department = "HR"
+                },
+                new Employee()
+                {
+                    EmployeeId = 3, EmployeeName = "Rahul Rathor", Address = "Laxmi Nagar", Department = "IT"
+                },
+                new Employee()
+                {
+                    EmployeeId = 4, EmployeeName = "YaduVeer Singh", Address = "Goa", Department = "Sales"
+                },
+                new Employee()
+                {
+                    EmployeeId = 5, EmployeeName = "Manish Sharma", Address = "New Delhi", Department = "HR"
+                },
+        };
+
+        /// <summary>
+        /// Gets all employees
+        /// </summary>
+        /// <returns></returns>
+        public IList<Employee> GetAllEmployees()
+        {
+            //Return list of all employees  
+            return employees;
+        }
+
+        /// <summary>
+        /// Gets an employee by Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Employee GetEmployeeDetails(int id)
+        {
+            //Return a single employee detail  
+            var employee = employees.FirstOrDefault(e => e.EmployeeId == id);
+            if (employee == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+            return employee;
+        }
+
+        /// <summary>
+        /// Gets an employee by name
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public Employee GetEmployeeByName(string name)
+        {
+            var employee = employees.FirstOrDefault(e => e.EmployeeName.ToLower().Contains(name.ToLower()));
+            if(employee == null)
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            return employee;
+        }
+    }
+}
